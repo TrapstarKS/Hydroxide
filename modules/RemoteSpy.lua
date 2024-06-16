c322d79300ddebc911e98daaed970dd56b4b715d
Trapstar <101758758+TrapstarKS@users.noreply.github.com>
2024-06-16 18:32:26 -0300

    tt

--- modules/RemoteSpy.lua
+++ modules/RemoteSpy.lua
@@ -163,7 +163,12 @@ if IsAnimeDefenders then
 			rawset(ModuleData, "Fire", newCClosure(function(...)
 				local instance = Remotes[_]
 				local method = "FireServer"
-				print(instance,ClassName)
+				print(instance.ClassName)
+
+				if instance.ClassName ~= "RemoteEvent" then
+					print("Skipped3 " .. MethodName)
+				end
+
 				if remotesViewing[instance.ClassName] and instance ~= remoteDataEvent and remoteMethods[method] then
 					local remote = currentRemotes[instance]
 					local vargs = { ... }
